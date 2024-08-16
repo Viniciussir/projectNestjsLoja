39f36137abedff433e59a29047f30adf92d223de
Vinicius C Scampini <viniciuscscampini@gmail.com>
2024-08-16 11:20:58 -0300

    Criação da pasta de dto e adicionado as validações de nome,email e senha

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,15 +1,16 @@
+import { CriaUsuarioDTO } from './dto/criausuario.dto';
 import { UsuarioRepositery } from './usuario.repositery';
 import { Body, Controller, Get, Post } from "@nestjs/common";
 
 @Controller('/usuarios')
 export class UsuarioController {
 
-    private usuarioRepositery = new UsuarioRepositery();
-
-    constructor(private usuarioRepository:UsuarioRepositery) {}
+    constructor(
+        private usuarioRepositery:UsuarioRepositery
+    ) {}
 
     @Post()
-    async criaUsuario(@Body()dadosUsuario) {
+    async criaUsuario(@Body()dadosUsuario: CriaUsuarioDTO) {
         this.usuarioRepositery.salvar(dadosUsuario)
         return dadosUsuario
         //return {status: 'usuario criado'}
